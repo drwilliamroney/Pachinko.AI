317097864add73f4b96870d5acbb917f91a247b4
Dr. William N. Roney <dr.william.roney@gmail.com>
2026-02-13 14:18:00 -0500

    Update and cleanup

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Pachinko.AI 
+Pachinko is a client / orchestration demonstrator that responds to AI Consumer needs to trust the work and output.  
+
+This solution focuses on using an LLM to generate code that utilizes MCP definitions to build python code which the AIRunners then execute.  The generated code is restricted to MCP functions, a handful of built-ins, and python native code constructs to answer the user's task.
+The system answers the consumer's trust-in-work by showing the code, showing a flowchart, and providing incremental updates as it proceeds.  This allows the consumer to treat the response in the same paradigm as a human aggregator of the response, being able to see where it went, how much information it retrieved, and the workflow used to compile the answer.  This is an analog to the "show your work" requirements in secondary-school math tests.
--- startup.bat
+++
@@ -1,7 +0,0 @@
-podman build -t pachinko -f Dockerfile .
-podman network create pachinko 
-podman run --detach --network pachinko -p 6817:6817 -v pachinko:/root/.pachinko --name slurmctl -h slurmctl pachinko  /bin/sh -c "/usr/local/bin/start_controller.sh"
-podman run --detach --network pachinko -p 8888:8888 -v pachinko:/root/.pachinko --name slurmjupyter -h slurmjupyter pachinko  /bin/sh -c "/usr/local/bin/start_jupyter.sh"
-podman run --detach --network pachinko -v pachinko:/root/.pachinko --name slurmnode1 -h slurmnode1 --memory=4g --cpus=2 pachinko  /bin/sh -c "/usr/local/bin/start_node.sh"
-podman run --detach --network pachinko -v pachinko:/root/.pachinko --name slurmnode2 -h slurmnode2 --memory=4g --cpus=2 pachinko  /bin/sh -c "/usr/local/bin/start_node.sh"
-podman run --detach --network pachinko --name ollama --gpus=all -p 11434:11434 -v ollama:/root/.ollama ollama
